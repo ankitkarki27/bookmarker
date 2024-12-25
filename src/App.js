0aa9dff43a31ce52d6d09ab6010aa0f95dd943cb
ankitkarki15 <ankitkarki8088@gmail.com>
2024-12-25 13:28:09 +0545

    First Commit

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import Header from './components/Header';
+import Stats from './components/Stats';
+import BookmarksList from './components/BookmarksList';
+import BookmarkForm from './components/BookmarkForm';
---
+++ src/Stats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Stats() {
+  return (
+    <section>
+      <p>Total Bookmarks: 10</p>
+    </section>
+  );
+}
+
+export default Stats;
